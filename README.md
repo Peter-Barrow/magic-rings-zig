3410df80e273ffacb3885bd33549de2f2f4b83c0
Peter Barrow <peter.barrow.93@gmail.com>
2024-11-19 19:43:51 +0100

    feat: intial root module

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 **Magic-Rings-Zig** is a small library to implement *magic ring buffers* available for Linux and Unix based platforms supporting either `memfd_create` or `shm_open` and `shm_unlink` as well as Windows.
 This ring buffer implementation makes use of a second mapping of the underlying data to allow reading off the end of buffer allocation and letting the OS and hardware take care of getting your cursor to wraparound to the beginning of the buffer.
 
-**STATUS: Unstable but feature complete (for now...)** This library does everything that I need it at the moment not to stay it won't change in the future. It has been built and tested with Zig 0.13.0 with plans to keep it line with subsequent releases.
+**STATUS: Unstable but feature complete (for now...)** This library does everything that I need at the moment not to stay it won't change in the future. It has been built and tested with Zig 0.13.0 with plans to keep it line with subsequent releases.
 
 ## Overview
 
-A typical ring buffer implementation requires the use of modulo division to find each index and is essentially just an arrya with context looking something like the following:
+A typical ring buffer implementation requires the use of modulo division to find each index and is essentially just an arrys with context looking something like the following:
 ```
 +---+---+---+---+---+---+---+---+---+---+
 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | <-- Buffer slots (indices)
@@ -23,7 +23,7 @@ Virtual Memory Layout:
 +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |
 +---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+---+
-  \___________ First Mapping ___________/  \____________Duplicate ____________/
+  \__________ First Mapping __________/   \_____________Duplicate ____________/
  ```
 Whilst maintaining an underlying memory layout like so.
 ```
--- src/root.zig
+++ src/root.zig
@@ -4,6 +4,90 @@ const magic_ring = switch (@import("builtin").target.os.tag) {
     else => @compileError("Platform not supported"),
 };
 
+const Map = @import("platforms/utilities.zig").MagicRingBase;
+const Mode = @import("platforms/utilities.zig").AccessMode;
+
+pub fn MagicRing(comptime T: type) type {
+    return struct {
+        const elem_size: u32 = @intCast(@sizeOf(T));
+
+        magic: Map,
+        data: []T,
+        capacity: usize,
+        count: usize,
+
+        pub fn new(num_elems: u32, name: []const u8) !@This() {
+            const size_in_bytes: u32 = num_elems * elem_size;
+            const magic = try magic_ring.create(name, size_in_bytes);
+            var data_as_manyT: [*]T = @ptrCast(magic.buffer);
+
+            return .{
+                .magic = magic,
+                .data = data_as_manyT[0 .. 2 * num_elems],
+                .capacity = num_elems,
+                .count = 0,
+            };
+        }
+
+        pub fn destroy(self: *@This()) !void {
+            try magic_ring.close(&self.magic);
+        }
+
+        // pub fn connect(name: []const u8, mode: Mode) !@This() {
+        //     const magic = try magic_ring.connect(name, mode);
+        //     var data_as_manyT: [*]T = @ptrCast(magic.buffer);
+        //     return .{
+        //         .magic = magic,
+        //         .data = data_as_manyT[0 .. 2 * num_elems],
+        //         .capacity = num_elems,
+        //         .count = 0,
+        //     };
+        // }
+
+        pub fn slice(self: *@This(), start: usize, stop: usize) ![]T {
+            if (start > stop) {
+                return error.StartAfterStop;
+            }
+
+            const absolute_start = @mod(start, self.capacity);
+            const total = stop - start;
+            const absolute_stop = absolute_start + total;
+
+            return self.data[absolute_start..absolute_stop];
+        }
+
+        pub fn slice_const(self: *@This(), start: usize, stop: usize) ![]const T {
+            if (start > stop) {
+                return error.StartAfterStop;
+            }
+
+            const absolute_start = @mod(start, self.capacity);
+            const total = stop - start;
+            const absolute_stop = absolute_start + total;
+
+            return @ptrCast(self.data[absolute_start..absolute_stop]);
+        }
+
+        pub fn push(self: *@This(), data: []T) !usize {
+            if (data.len > self.capacity) {
+                return error.LargerThanBuffer;
+            }
+
+            var buffer_section = self.slice(self.capacity, self.capacity + data.len);
+            @memcpy(&buffer_section, data);
+
+            return data.len;
+        }
+
+        pub fn zero(self: *@This()) void {
+            for (0..self.capacity) |i| {
+                self.data[i] = undefined;
+            }
+            self.count = 0;
+        }
+    };
+}
+
 test {
     _ = magic_ring;
 }
