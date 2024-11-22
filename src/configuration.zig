21928684a85a9caf647744aa7f505697a4df75d9
Peter Barrow <peter.barrow.93@gmail.com>
2024-11-22 18:07:07 +0100

    feat: creation of config files
    config files can now be created, these are written to a local
    configuration folder for the users project, this will be used with the
    "connect" functionality to check for the existence of other magic rings

---
+++ src/configuration.zig
@@ -0,0 +1,169 @@
+const std = @import("std");
+const knownFolders = @import("known-folders");
+const MAX_PATH_LENGTH = std.fs.max_path_bytes;
+const MAX_NAME_LENGTH = std.fs.MAX_NAME_BYTES;
+
+pub fn MakeConfig(comptime project_name: []const u8, comptime ElementType: type) type {
+    return struct {
+        const Self = @This();
+
+        /// Configuration structure for the application
+        ///
+        /// Fields:
+        ///   name: Name of the configuration
+        ///   shm_path: Path to shared memory
+        ///   num_connections: Number of connections
+        ///   library_version: Version of the library
+        ///   shm_size: Size of shared memory
+        project_name: []const u8 = project_name,
+        name: []const u8,
+        shm_path: []const u8,
+        num_connections: usize,
+        library_version: []const u8 = "1.0.0", // Replace "1.0.0" with the actual version of your library
+        shm_size: usize,
+
+        // Comptime fields derived from ElementType
+        const element_size: comptime_int = @sizeOf(ElementType);
+        const element_type: []const u8 = @typeName(ElementType);
+
+        /// Initialize a new configuration instance
+        ///
+        /// Args:
+        ///   name: Name of the configuration
+        ///   shm_path: Path to shared memory
+        ///   shm_size: Size of shared memory
+        ///
+        /// Returns:
+        ///   A new instance of the configuration struct
+        pub fn init(
+            name: []const u8,
+            shm_path: []const u8,
+            shm_size: usize,
+        ) Self {
+            return Self{
+                .name = name,
+                .shm_path = shm_path,
+                .num_connections = 0,
+                .shm_size = shm_size,
+            };
+        }
+
+        /// Generates the full path for the configuration file
+        ///
+        /// Args:
+        ///   allocator: Memory allocator to use for string operations
+        ///   name: Name of the configuration file
+        ///
+        /// Returns:
+        ///   The full path to the configuration file as a string
+        fn makeFilePath(allocator: std.mem.Allocator, name: []const u8) ![]const u8 {
+            const config_dir = blk: {
+                // knownFolders.getPath(...) can return null, this however should never happen
+                // in the case it does raise an error
+                const conf_dir = try knownFolders.getPath(allocator, .local_configuration);
+                if (conf_dir) |cf| break :blk cf;
+
+                return error.NoLocalConfigurationForSystem;
+            };
+
+            defer allocator.free(config_dir);
+
+            const app_config_dir = try std.fs.path.join(
+                allocator,
+                &[_][]const u8{ config_dir, project_name },
+            );
+            defer allocator.free(app_config_dir);
+
+            const file_name = try std.fmt.allocPrint(allocator, "{s}_config.json", .{name});
+            defer allocator.free(file_name);
+
+            std.debug.assert(file_name.len <= MAX_NAME_LENGTH);
+
+            const file_path = try std.fs.path.join(
+                allocator,
+                &[_][]const u8{ app_config_dir, file_name },
+            );
+
+            std.debug.assert(file_path.len <= MAX_PATH_LENGTH);
+
+            return file_path;
+        }
+
+        /// Converts the Config struct to a JSON string
+        ///
+        /// Args:
+        ///   allocator: Memory allocator to use for JSON string creation
+        ///
+        /// Returns:
+        ///   A JSON string representation of the Config struct
+        pub fn toJson(self: *const Self, allocator: std.mem.Allocator) ![]const u8 {
+            const json_value = try std.json.stringify(self, .{}, allocator);
+            return json_value;
+        }
+
+        /// Writes the Config struct to a JSON file in the local configuration directory
+        ///
+        /// Args:
+        ///   allocator: Memory allocator to use for file operations
+        ///
+        /// Returns:
+        ///   void, or an error if the write operation fails
+        pub fn writeConfig(self: *const Self, allocator: std.mem.Allocator) !void {
+            const file_path = try Self.makeFilePath(allocator, self.name);
+            defer allocator.free(file_path);
+
+            const json_data = try self.toJson(allocator);
+            defer allocator.free(json_data);
+
+            try std.fs.cwd().writeFile(file_path, json_data);
+        }
+
+        /// Loads a Config struct from a JSON file in the local configuration directory
+        ///
+        /// Args:
+        ///   allocator: Memory allocator to use for file operations and JSON parsing
+        ///   name: Name of the configuration file to load
+        ///
+        /// Returns:
+        ///   A Config struct populated with data from the JSON file, or an error if the load operation fails
+        pub fn loadConfig(allocator: std.mem.Allocator, name: []const u8) !Self {
+            const file_path = try Self.makeFilePath(allocator, name);
+            defer allocator.free(file_path);
+
+            const file_contents = try std.fs.cwd().readFileAlloc(
+                allocator,
+                file_path,
+                std.math.maxInt(usize),
+            );
+            defer allocator.free(file_contents);
+
+            var stream = std.json.TokenStream.init(file_contents);
+            return try std.json.parse(Self, &stream, .{ .allocator = allocator });
+        }
+    };
+}
+
+test "getConfigFilePath" {
+    const TestConfig = MakeConfig("test_project", u32);
+    const allocator = std.testing.allocator;
+
+    const test_config = TestConfig.init("test_config", "/tmp/test_shm", 1024);
+    const file_path = try TestConfig.makeFilePath(allocator, test_config.name);
+    defer allocator.free(file_path);
+
+    var buffer: [std.fs.max_path_bytes]u8 = undefined;
+    const target = try std.fmt.bufPrintZ(
+        &buffer,
+        "{s}{s}{s}_config.json",
+        .{
+            test_config.project_name,
+            [1]u8{std.fs.path.sep},
+            test_config.name,
+        },
+    );
+
+    std.debug.print("Config Path:\t{s}\n", .{target});
+    std.debug.print("Config Path:\t{s}\n", .{file_path});
+
+    try std.testing.expect(std.mem.endsWith(u8, file_path, target));
+}
--- src/root.zig
+++ src/root.zig
@@ -6,93 +6,96 @@ const magic_ring = switch (@import("builtin").target.os.tag) {
     else => @compileError("Platform not supported"),
 };
 
-const Map = @import("platforms/utilities.zig").MagicRingBase;
-const Mode = @import("platforms/utilities.zig").AccessMode;
+const config = @import("configuration.zig");
 
-pub fn MagicRing(comptime T: type) type {
-    return struct {
-        const Self = @This();
-        const elem_size: u32 = @intCast(@sizeOf(T));
-
-        magic: Map,
-        data: []T,
-        capacity: usize,
-        count: usize,
-
-        pub fn new(num_elems: u32, name: []const u8) !Self {
-            const size_in_bytes: u32 = num_elems * elem_size;
-            const magic = try magic_ring.create(name, size_in_bytes);
-            var data_as_manyT: [*]T = @ptrCast(magic.buffer);
-
-            return .{
-                .magic = magic,
-                .data = data_as_manyT[0 .. 2 * num_elems],
-                .capacity = num_elems,
-                .count = 0,
-            };
-        }
-
-        pub fn destroy(self: *Self) !void {
-            try magic_ring.close(&self.magic);
-        }
-
-        pub fn connect(name: []const u8, mode: Mode) !Self {
-            const magic = try magic_ring.connect(name, mode);
-            var data_as_manyT: [*]T = @ptrCast(magic.buffer);
-            const num_elems = @divExact(magic.buffer.len, elem_size);
-
-            return .{
-                .magic = magic,
-                .data = data_as_manyT[0 .. 2 * num_elems],
-                .capacity = num_elems,
-                .count = 0,
-            };
-        }
-
-        pub fn slice(self: *Self, start: usize, stop: usize) ![]T {
-            if (start > stop) {
-                return error.StartAfterStop;
-            }
-
-            const absolute_start = @mod(start, self.capacity);
-            const total = stop - start;
-            const absolute_stop = absolute_start + total;
-
-            return self.data[absolute_start..absolute_stop];
-        }
-
-        pub fn slice_const(self: *Self, start: usize, stop: usize) ![]const T {
-            if (start > stop) {
-                return error.StartAfterStop;
-            }
-
-            const absolute_start = @mod(start, self.capacity);
-            const total = stop - start;
-            const absolute_stop = absolute_start + total;
-
-            return @ptrCast(self.data[absolute_start..absolute_stop]);
-        }
-
-        pub fn push(self: *Self, data: []T) !usize {
-            if (data.len > self.capacity) {
-                return error.LargerThanBuffer;
-            }
-
-            var buffer_section = self.slice(self.capacity, self.capacity + data.len);
-            @memcpy(&buffer_section, data);
-
-            return data.len;
-        }
-
-        pub fn zero(self: *Self) void {
-            for (0..self.capacity) |i| {
-                self.data[i] = undefined;
-            }
-            self.count = 0;
-        }
-    };
-}
+// const Map = @import("platforms/utilities.zig").MagicRingBase;
+// const Mode = @import("platforms/utilities.zig").AccessMode;
+//
+// pub fn MagicRing(comptime T: type) type {
+//     return struct {
+//         const Self = @This();
+//         const elem_size: u32 = @intCast(@sizeOf(T));
+//
+//         magic: Map,
+//         data: []T,
+//         capacity: usize,
+//         count: usize,
+//
+//         pub fn new(num_elems: u32, name: []const u8) !Self {
+//             const size_in_bytes: u32 = num_elems * elem_size;
+//             const magic = try magic_ring.create(name, size_in_bytes);
+//             var data_as_manyT: [*]T = @ptrCast(magic.buffer);
+//
+//             return .{
+//                 .magic = magic,
+//                 .data = data_as_manyT[0 .. 2 * num_elems],
+//                 .capacity = num_elems,
+//                 .count = 0,
+//             };
+//         }
+//
+//         pub fn destroy(self: *Self) !void {
+//             try magic_ring.close(&self.magic);
+//         }
+//
+//         pub fn connect(name: []const u8, mode: Mode) !Self {
+//             const magic = try magic_ring.connect(name, mode);
+//             var data_as_manyT: [*]T = @ptrCast(magic.buffer);
+//             const num_elems = @divExact(magic.buffer.len, elem_size);
+//
+//             return .{
+//                 .magic = magic,
+//                 .data = data_as_manyT[0 .. 2 * num_elems],
+//                 .capacity = num_elems,
+//                 .count = 0,
+//             };
+//         }
+//
+//         pub fn slice(self: *Self, start: usize, stop: usize) ![]T {
+//             if (start > stop) {
+//                 return error.StartAfterStop;
+//             }
+//
+//             const absolute_start = @mod(start, self.capacity);
+//             const total = stop - start;
+//             const absolute_stop = absolute_start + total;
+//
+//             return self.data[absolute_start..absolute_stop];
+//         }
+//
+//         pub fn slice_const(self: *Self, start: usize, stop: usize) ![]const T {
+//             if (start > stop) {
+//                 return error.StartAfterStop;
+//             }
+//
+//             const absolute_start = @mod(start, self.capacity);
+//             const total = stop - start;
+//             const absolute_stop = absolute_start + total;
+//
+//             return @ptrCast(self.data[absolute_start..absolute_stop]);
+//         }
+//
+//         pub fn push(self: *Self, data: []T) !usize {
+//             if (data.len > self.capacity) {
+//                 return error.LargerThanBuffer;
+//             }
+//
+//             var buffer_section = self.slice(self.capacity, self.capacity + data.len);
+//             @memcpy(&buffer_section, data);
+//
+//             return data.len;
+//         }
+//
+//         pub fn zero(self: *Self) void {
+//             for (0..self.capacity) |i| {
+//                 self.data[i] = undefined;
+//             }
+//             self.count = 0;
+//         }
+//     };
+// }
 
 test {
     _ = magic_ring;
+    _ = config;
 }
