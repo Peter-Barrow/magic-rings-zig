bc3ba961e4810e89814a7ec5e03150dcca411237
Peter Barrow <peter.barrow.93@gmail.com>
2025-07-26 19:19:48 +0200

    build: packaging

---
+++ src/magicrings.zig
@@ -0,0 +1,8 @@
+const magic_ring = @import("magic_ring.zig");
+const multi_magic_ring = @import("multi_magic_rings.zig");
+
+
+test {
+    _ = magic_ring;
+    _ = multi_magic_ring;
+}
--- src/root.zig
+++
@@ -1,8 +0,0 @@
-const magic_ring = @import("./magic_ring.zig");
-const multi_magic_ring = @import("./multi_magic_rings.zig");
-
-// test {
-//     const std = @import("std");
-//     std.testing.refAllDecls(magic_ring);
-//     std.testing.refAllDecls(multi_magic_ring);
-// }
