a0162209d9e13e5130250d692e5dc6b5a1bb7224
Peter Barrow <peter.barrow.93@gmail.com>
2026-04-05 12:35:02 +0200

    chore: tidy up imports

--- magic_rings.zig
+++ magic_rings.zig
@@ -1,7 +1,7 @@
 const magic_ring = @import("src/magic_ring.zig");
 const multi_magic = @import("src/multi_magic_rings.zig");
 
-pub const MagicRing = magic_ring.MagicRingWithHeader;
+pub const MagicRing = magic_ring.MagicRing;
 pub const MultiMagicRing = multi_magic.MultiMagicRing;
 
 test "ref all decls" {
