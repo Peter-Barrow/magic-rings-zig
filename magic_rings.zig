9b3fcd862a5f11c3a7df3c12f5b4549217c0de90
Peter Barrow <peter.barrow.93@gmail.com>
2026-04-06 17:28:52 +0200

    fix: make state type public

--- magic_rings.zig
+++ magic_rings.zig
@@ -1,6 +1,7 @@
 const magic_ring = @import("src/magic_ring.zig");
 const multi_magic = @import("src/multi_magic_rings.zig");
 
+pub const State = magic_ring.State;
 pub const MagicRing = magic_ring.MagicRing;
 pub const MultiMagicRing = multi_magic.MultiMagicRing;
 
